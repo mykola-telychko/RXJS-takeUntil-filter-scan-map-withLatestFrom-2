fa8ea49fb7d7f6b82a8f7df5be26df16b349defc
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-24 17:29:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-takeUntil-filter-scan-map-withLatestFrom-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-jxmo1f)
---
+++ index.ts
@@ -0,0 +1,29 @@
+import { interval } from 'rxjs';
+import { takeUntil, filter, scan, map, withLatestFrom } from 'rxjs/operators';
+
+//emit value every 1s
+const srcInterval$ = interval(1000);
+//is number even?
+const isEven = (val) => val % 2 === 0;
+//only allow values that are even
+const evenSource = srcInterval$.pipe(filter(isEven));
+//keep a running total of the number of even numbers out
+const evenNumberCount = evenSource.pipe(scan((acc, _) => acc + 1, 0));
+//do not emit until 5 even numbers have been emitted
+const fiveEvenNumbers = evenNumberCount.pipe(filter((val) => val > 5));
+
+const example = evenSource.pipe(
+  //also give me the current even number count for display
+  withLatestFrom(evenNumberCount),
+  map(([val, count]) => `Even number (${count}) : ${val}`),
+  //when five even numbers have been emitted, complete source observable
+  takeUntil(fiveEvenNumbers)
+);
+const subscribe = example.subscribe((val) => console.log(val));
+/*
+	Even number (1) : 0,
+  Even number (2) : 2
+	Even number (3) : 4
+	Even number (4) : 6
+	Even number (5) : 8
+*/
